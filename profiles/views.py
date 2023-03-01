51677acfe0139a34134f0d32d9de93f43a3ebd95
Evan Tung <dragonejt@gmail.com>
2023-03-01 16:21:28 -0700

    change permissions

--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from profiles.models import UserProfile, ServerProfile, UserProfileSerializer, ServerProfileSerializer
 # Create your views here.
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def ingest_message(request: Request) -> Response:
     print(request.data)
     return Response(status=status.HTTP_200_OK)
--- sibyl/settings.py
+++ sibyl/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer'

7d176c3216437a7b11e9405f8839303b5a20f160
Evan Tung <dragonejt@gmail.com>
2023-03-01 20:49:13 -0700

    fix urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path("user/", views.UserProfiles.as_view()),
-    path("message/", views.ingest_message)
+    path("user", views.UserProfiles.as_view()),
+    path("message", views.ingest_message)
 ]

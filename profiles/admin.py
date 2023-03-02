ea0b70819be3f2677c81f97a02c2dc7f555edaa8
Evan Tung <dragonejt@gmail.com>
2023-03-01 20:09:06 -0700

    add profiles models to admin site

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from profiles.models import UserProfile, CommunityProfile
 
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(CommunityProfile)

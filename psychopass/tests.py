8b97ebd2e2abe50dadfe3adad086040fd862544d
Evan Tung <dragonejt@gmail.com>
2023-03-22 19:21:25 -0700

    add test for ingest_message

--- psychopass/tests.py
+++ psychopass/tests.py
@@ -1,19 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from django.utils.crypto import get_random_string
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient
-
 from psychopass.views import UserPsychoPass, serialize_user, CommunityPsychoPass, serialize_community
 
 # Create your tests here.
 
 
-class TestUserPsychoPassView(TestCase):
+class TestUserPsychoPassView(APITestCase):
 
     def setUp(self) -> None:
-        self.client = APIClient()
+        self.url = "/psychopass/user"
         self.user = User.objects.create_superuser(username="discord")
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
@@ -22,14 +20,14 @@ class TestUserPsychoPassView(TestCase):
 
     def test_get(self) -> None:
         response = self.client.get(
-            f"/psychopass/user?id={self.psycho_pass.platform_id}", format="json")
+            f"{self.url}?id={self.psycho_pass.platform_id}", format="json")
         self.assertEqual(response.json(), serialize_user(self.psycho_pass))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post(self) -> None:
         user_id = get_random_string(20)
         response = self.client.post(
-            "/psychopass/user", data={"userID": user_id}, format="json")
+            self.url, data={"userID": user_id}, format="json")
         psycho_pass = UserPsychoPass.objects.get(platform_id=user_id)
         self.assertEqual(response.json().get("platform_id"), user_id)
         self.assertEqual(psycho_pass.platform_id, user_id)
@@ -39,16 +37,16 @@ class TestUserPsychoPassView(TestCase):
         user_id = self.psycho_pass.platform_id
         self.assertEqual(self.psycho_pass.platform_id, user_id)
         response = self.client.delete(
-            f"/psychopass/user?id={self.psycho_pass.platform_id}")
+            f"{self.url}?id={self.psycho_pass.platform_id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaises(UserPsychoPass.DoesNotExist):
             UserPsychoPass.objects.get(platform_id=user_id)
 
 
-class TestCommunityPsychoPassView(TestCase):
+class TestCommunityPsychoPassView(APITestCase):
 
     def setUp(self) -> None:
-        self.client = APIClient()
+        self.url = "/psychopass/community"
         self.user = User.objects.create_superuser(username="discord")
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
@@ -57,7 +55,7 @@ class TestCommunityPsychoPassView(TestCase):
 
     def test_get(self) -> None:
         response = self.client.get(
-            f"/psychopass/community?id={self.psycho_pass.platform_id}", format="json")
+            f"{self.url}?id={self.psycho_pass.platform_id}", format="json")
         self.assertEqual(
             response.json(), serialize_community(self.psycho_pass))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -65,7 +63,7 @@ class TestCommunityPsychoPassView(TestCase):
     def test_post(self) -> None:
         community_id = get_random_string(20)
         response = self.client.post(
-            "/psychopass/community", data={"communityID": community_id}, format="json")
+            self.url, data={"communityID": community_id}, format="json")
         psycho_pass = CommunityPsychoPass.objects.get(platform_id=community_id)
         self.assertEqual(response.json().get("platform_id"), community_id)
         self.assertEqual(psycho_pass.platform_id, community_id)
@@ -75,7 +73,69 @@ class TestCommunityPsychoPassView(TestCase):
         community_id = self.psycho_pass.platform_id
         self.assertEqual(self.psycho_pass.platform_id, community_id)
         response = self.client.delete(
-            f"/psychopass/community?id={self.psycho_pass.platform_id}")
+            f"{self.url}?id={self.psycho_pass.platform_id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaises(CommunityPsychoPass.DoesNotExist):
             CommunityPsychoPass.objects.get(platform_id=community_id)
+
+
+class TestIngestMessage(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = "/psychopass/message"
+        self.user = User.objects.create_superuser(username="discord")
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
+
+    def test_ingest_message(self) -> None:
+        response = self.client.post(self.url, data={
+            "attributeScores": {
+                "TOXICITY": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                },
+                "SEVERE_TOXICITY": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                },
+                "IDENTITY_ATTACK": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                },
+                "INSULT": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                },
+                "THREAT": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                },
+                "PROFANITY": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                },
+                "SEXUALLY_EXPLICIT": {
+                    "summaryScore": {
+                        "value": 0.5,
+                        "type": "PROBABILITY"
+                    }
+                }
+            },
+            "languages": ["en"],
+            "userID": get_random_string(20),
+            "communityID": get_random_string(20)
+        }, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)

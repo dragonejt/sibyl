a1449d560b00c93d60f72506d2f37057c6030260
Evan Tung <dragonejt@gmail.com>
2023-04-20 21:21:44 -0700

    change Community verbose name

--- community/models.py
+++ community/models.py
@@ -9,14 +9,18 @@ class Community(models.Model):
     platform = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     community_id = models.CharField(max_length=20, unique=True)
 
+    class Meta:
+        verbose_name = "Community"
+        verbose_name_plural = "Communities"
+
+    def __str__(self) -> str:
+        return f"{self.platform.username}/{self.community_id}"
+
     # For Discord Servers Only
     discord_log_channel = models.CharField(
         max_length=20, blank=True, null=True)
     discord_notify_target = models.CharField(
         max_length=20, blank=True, null=True)
-
-    def __str__(self) -> str:
-        return f"{self.platform.username}/{self.community_id}"
 
 
 class CommunitySerializer(ModelSerializer):
